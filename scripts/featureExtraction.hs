caadae8ae5c66189de35eca3746f9841dd880f32
Victor Lindhe <victorlindhe@gmail.com>
2016-03-10 15:50:49 +0100

    Shift computer commit

--- scripts/featureExtraction.hs
+++ scripts/featureExtraction.hs
@@ -14,7 +14,9 @@ data Features a = Features
     { features :: [a] 
     } deriving (Eq,Ord,Show)
 
-data Feature a = SingleFeature a
+data Feature a = SingleFeature a -- One lonely symbol
+    | MultiFeature a [Feature a] -- Symbol with inner symbols
+    deriving (Eq,Ord,Show)
 
 emptyFeatures :: Features (Feature Id)
 emptyFeatures = Features []
@@ -23,6 +25,10 @@ mergeFeatures :: Features (Feature Id) -> Features (Feature Id) -> Features (Fea
 mergeFeatures (Features fs1) (Features []) = Features fs1
 mergeFeatures f1@(Features fs1) (Features (f:fs2)) = mergeFeatures (addFeature f1 f) (Features fs2)
 
+getFeature :: Features (Feature Id) -> Maybe (Feature Id)
+getFeature (Features []) = Nothing
+getFeature (Features (x:xs)) = Just x
+
 addFeature :: Features (Feature Id) -> Feature Id -> Features (Feature Id)
 addFeature (Features fs) f
     | any (hasFeature f) fs = Features fs
@@ -30,11 +36,20 @@ addFeature (Features fs) f
 
 hasFeature :: Feature Id -> Feature Id -> Bool
 hasFeature (SingleFeature a) (SingleFeature b) = (idUnique a) == (idUnique b)
+hasFeature _ _ = False
+
+extractId :: Feature Id -> Id
+extractId (SingleFeature i) = i
+extractId (MultiFeature i _) = i
 
 printFeatures :: Features (Feature Id) -> IO ()
 printFeatures (Features []) = return ()
 printFeatures (Features ((SingleFeature f):fs)) = do
     putStrLn $ idString f
+    printFeatures (Features fs)
+
+printFeatures (Features ((MultiFeature f fs2):fs)) = do
+    putStrLn $ (idString f) ++ " " ++ (idString $ extractId $ head fs2)
     printFeatures (Features fs)
 
 -- Reads a library file to begin with
@@ -69,15 +84,15 @@ readFeature (f:xs) = do
 
     fs <- extractFromExpr (fm_body f) fs
     printFeatures fs
-    readFeature xs
+    -- readFeature xs
 
-extractFromExpressions :: [Expr Id] -> Features (Feature Id) -> IO ( Features (Feature Id))
+extractFromExpressions :: [Expr Id] -> Features (Feature Id) -> IO (Features (Feature Id))
 extractFromExpressions [] fs = do return fs
 extractFromExpressions (x:xs) fs = do
     fs1 <- extractFromExpr x fs
     return fs1
 
-extractFromExpr :: Expr Id -> Features (Feature Id) -> IO ( Features (Feature Id))
+extractFromExpr :: Expr Id -> Features (Feature Id) -> IO (Features (Feature Id))
 extractFromExpr expr fs = do
     case expr of
 
@@ -92,10 +107,18 @@ extractFromExpr expr fs = do
             fs2 <- extractFromExpr exp2 fs
             return $ mergeFeatures fs1 fs2
 
-        -- Use of global function?
+        -- Use of global function
+        -- This one is tricky
+        -- For example, xs ++ ys we want to generate the following 4 features:
+        -- `++`, `list ++`, `++ list` and `list ++ list`.
+        -- Currently 
         Gbl (Global name typ args) :@: exps -> do
-            fs1 <- extractFromExpressions exps fs
-            return $ addFeature fs1 (SingleFeature name)
+            f1@(Features fs1) <- extractFromExpressions exps fs
+            let fs2 = addFeature f1 (SingleFeature name)
+            case (length exps) of
+                0 -> return fs2
+                1 -> return $ addFeature fs2 (MultiFeature name [head fs1])
+                2 -> return $ addFeature (addFeature fs2 (MultiFeature name [head fs1])) (MultiFeature name [last fs1])
 
         -- Local variable, we are looking for the type
         Lcl (Local name (TyCon feature _)) -> do
