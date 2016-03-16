317508d6d1cb48885a8797840614851c26ffa47e
Victor Lindhe <victorlindhe@gmail.com>
2016-03-16 11:03:59 +0100

    Extracting sub trees work

--- scripts/featureExtraction.hs
+++ scripts/featureExtraction.hs
@@ -14,6 +14,7 @@ import Control.Monad
 
 -- Tree structure
 data FNode a = FNode a [FNode a]
+    deriving (Eq,Ord,Show)
 
 -- Reads a library file to begin with
 main :: IO ()
@@ -40,15 +41,23 @@ readLibrary (f:xs) = do
     putStrLn $ "--------------------------------------"
     pprint f
     putStrLn $ "--------------------------------------"
-    printTree " " $ buildTree (fm_body f)
+    let trees = extractSubTrees 2 (buildTree (fm_body f))
+    
     -- readLibrary xs
 
+-- Printing a tree using indentation
 printTree :: String -> FNode Id -> IO ()
 printTree sep (FNode id fs) = do putStrLn $ sep ++ idString id; mapM_ (printTree (sep ++ "  ")) fs
     
+-- Builds a tree for an expression, recursively :)
 buildTree :: Expr Id -> FNode Id
 buildTree (Quant _ _ _ e') = buildTree e'
 buildTree (Builtin Equal :@: exps) = FNode (Id "==" 0 (Just (0,0))) $ map buildTree exps
 buildTree (Lcl (Local name (TyCon feature _))) = FNode feature []
 buildTree (Gbl (Global name typ args) :@: exps) = FNode name $ map buildTree exps
 buildTree _ = FNode (Id "unknown" 0 (Just (0,0))) []
+
+-- Extract subtrees with certain depth. Returns all possible trees as a list
+extractSubTrees :: Int -> FNode Id -> [FNode Id]
+extractSubTrees 1 (FNode id fs) = [(FNode id [])]
+extractSubTrees depth (FNode id fs) = [(FNode id (concat $ map (extractSubTrees (depth-1)) fs))] ++ (concat $ map (extractSubTrees depth) fs)
