27ff44e00c1dd3d1d12abaef0333601be40ae710
Victor Lindhe <victorlindhe@gmail.com>
2016-03-16 10:48:36 +0100

    Rewriting the entire script using tree structure instead

--- scripts/featureExtraction.hs
+++ scripts/featureExtraction.hs
@@ -10,16 +10,10 @@ import Tip.Pretty
 import Data.Maybe
 import Data.List
 import Debug.Trace
+import Control.Monad
 
--- Datatype for listing the features of a formula
-data Features a = Features
-    { features :: [a] 
-    } deriving (Eq,Ord,Show)
-
-data Feature a = SingleFeature a -- One lonely symbol
-    | MultiFeature a [Feature a] -- Symbol with inner symbols
-    | EmptyFeature -- Not defined symbol
-    deriving (Eq,Ord,Show)
+-- Tree structure
+data FNode a = FNode a [FNode a]
 
 -- Reads a library file to begin with
 main :: IO ()
@@ -32,126 +26,29 @@ main = do
         True -> do
             libraryString <- readFile filePath
             case parseLibrary libraryString of
-                Left msg      -> error $ "parsing library failed:"++show msg
-                Right library -> do
-                    putStrLn "loaded library"
-                    saveLibrary library
+                Left msg      -> error $ "Parsing library failed:"++show msg
+                Right (Library _ _ ls) -> do
+                    putStrLn "Loaded library"
+                    readLibrary (M.elems ls)
 
-emptyFeatures :: Features (Feature Id)
-emptyFeatures = Features []
-
-mergeFeatures :: Features (Feature Id) -> Features (Feature Id) -> Features (Feature Id)
-mergeFeatures (Features fs1) (Features []) = Features fs1
-mergeFeatures f1@(Features fs1) (Features (f:fs2)) = mergeFeatures (addFeature f1 f) (Features fs2)
-
-addFeature :: Features (Feature Id) -> Feature Id -> Features (Feature Id)
-addFeature (Features fs) f
-    | any (equalsFeature f) fs = Features fs
-    | otherwise = Features $ (fs++[f])
-
-addFeatures :: Features (Feature Id) -> [Feature Id] -> Features (Feature Id)
-addFeatures f' [] = f'
-addFeatures f' (f:fs) = addFeature (addFeatures f' fs) f
-
-equalsFeature :: Feature Id -> Feature Id -> Bool
-equalsFeature (SingleFeature a) (SingleFeature b) = (idUnique a) == (idUnique b)
-equalsFeature _ _ = False
-
-printFeatures :: Features (Feature Id) -> IO ()
-printFeatures (Features []) = return ()
-printFeatures (Features (f:fs)) = do
-    putStrLn $ featureToString f
-    printFeatures (Features fs)
-
-featureToString :: Feature Id -> String
-featureToString EmptyFeature = "_"
-featureToString (SingleFeature f) = idString f
-featureToString (MultiFeature f []) = idString f
-featureToString (MultiFeature f fs) = idString f ++ "(" ++ (featuresToString fs " ") ++ ")"
-
-featuresToString :: [Feature Id] -> String -> String
-featuresToString [] _ = ""
-featuresToString [f] sep = (featureToString f)
-featuresToString (f:fs) sep = (featureToString f) ++ sep ++ (featuresToString fs sep)
-
-saveLibrary :: Library Id -> IO ()
-saveLibrary (Library _ _ ls) = do 
-    readFeature $ M.elems ls
-
-readFeature :: [Formula Id] -> IO ()
-readFeature [] = return ()
-readFeature (f:xs) = do
-    let fs = emptyFeatures
+-- Going through each lemma of the library
+readLibrary :: [Formula Id] -> IO ()
+readLibrary [] = return ()
+readLibrary (f:xs) = do
     putStrLn "\n#######################################"
     putStrLn $ "Looking at lemma " ++ (fromJust $ getFmName f)
     putStrLn $ "--------------------------------------"
     pprint f
     putStrLn $ "--------------------------------------"
+    printTree " " $ buildTree (fm_body f)
+    -- readLibrary xs
 
-    fs <- extractFromExpr (fm_body f) fs
-    putStrLn "Blaha"
-    --printFeatures fs
-    readFeature xs
-
-extractFromExpressions :: [Expr Id] -> Features (Feature Id) -> IO (Features (Feature Id))
-extractFromExpressions [] fs = do return fs
-extractFromExpressions (x:xs) fs = do
-    fs1 <- extractFromExpr x fs
-    fs2 <- extractFromExpressions xs fs1 
-    return fs2
-
-extractFromExpr :: Expr Id -> Features (Feature Id) -> IO (Features (Feature Id))
-extractFromExpr expr fs = do
-    case expr of
-
-        -- Top level expression, with the quantifier. We just go down looking for more expressions
-        Quant _ _ _ expr -> do
-            fs1 <- extractFromExpr expr fs
-            return fs1
-
-        -- Equality between two expressions. We extract features from both :)
-        Builtin Equal :@: [exp1, exp2] -> do
-            fs1 <- extractFromExpr exp1 fs
-            fs2 <- extractFromExpr exp2 fs
-            return $ mergeFeatures fs1 fs2
-
-        -- Use of global function
-        -- This one is tricky
-        -- For example, xs ++ ys we want to generate the following 4 features:
-        -- `++`, `list ++`, `++ list` and `list ++ list`.
-        -- Currently 
-        Gbl (Global name typ args) :@: exps -> do
-            f1 <- extractFromExpressions exps fs
-            let fargs = extractArgumentFeatures (features f1)
-            let f2 = createMultiFeatures emptyFeatures fargs name (length exps)
-            putStrLn "----- f1:"
-            printFeatures f1
-            putStrLn "----- f2:"
-            printFeatures f2
-            return $ addFeature f1 (MultiFeature name (take (length exps) (repeat EmptyFeature)))
-
-        -- Local variable, we are looking for the type
-        Lcl (Local name (TyCon feature _)) -> do
-            return $ addFeature fs (SingleFeature feature)
-
-        _ -> do
-            -- putStrLn $ show expr
-            return fs
-
-extractArgumentFeatures :: [Feature Id] -> [Feature Id]
-extractArgumentFeatures [] = []
-extractArgumentFeatures (f:fs) = [f] ++ (extractArgumentFeatures fs)
-
--- First step in generating the feature combos
-createMultiFeatures :: Features (Feature Id) -> [Feature Id] -> Id -> Int -> Features (Feature Id)
-createMultiFeatures f' [] name d = addFeature f' (MultiFeature name (take d (repeat EmptyFeature))) -- The empty case
-createMultiFeatures f' (f:fs) name d = addFeatures f' (generateCombos f fs name)
-
-generateCombos :: Feature Id -> [Feature Id] -> Id -> [Feature Id]
-generateCombos base [] name = [MultiFeature name [base], MultiFeature name [EmptyFeature]]
-generateCombos base (a:as) name = (extendCombos emptyCase (generateCombos a as name)) ++ (extendCombos baseCase (generateCombos a as name))
-    where 
-        baseCase = MultiFeature name [base]
-        emptyCase = MultiFeature name [EmptyFeature]
-        extendCombos b [] = []
-        extendCombos b@(MultiFeature name' [base']) ((MultiFeature _ f'):fs) = (MultiFeature name' (base':f')):(extendCombos b fs)
+printTree :: String -> FNode Id -> IO ()
+printTree sep (FNode id fs) = do putStrLn $ sep ++ idString id; mapM_ (printTree (sep ++ "  ")) fs
+    
+buildTree :: Expr Id -> FNode Id
+buildTree (Quant _ _ _ e') = buildTree e'
+buildTree (Builtin Equal :@: exps) = FNode (Id "==" 0 (Just (0,0))) $ map buildTree exps
+buildTree (Lcl (Local name (TyCon feature _))) = FNode feature []
+buildTree (Gbl (Global name typ args) :@: exps) = FNode name $ map buildTree exps
+buildTree _ = FNode (Id "unknown" 0 (Just (0,0))) []
