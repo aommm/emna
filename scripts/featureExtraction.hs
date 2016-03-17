c4b0649fadb0f7709bc4000775670dd3886e01eb
Victor Lindhe <victorlindhe@gmail.com>
2016-03-17 13:48:03 +0100

    Fixed depth 3+

--- scripts/featureExtraction.hs
+++ scripts/featureExtraction.hs
@@ -36,7 +36,9 @@ main = do
 readLibrary :: [Formula Id] -> IO ()
 readLibrary [] = return ()
 readLibrary (f:xs) = do
-    let features = nub $ concat $ map extractFeatures (extractSubTrees 2 (buildTree (fm_body f)))
+    let tree = buildTree (fm_body f)
+    let trees = extractSubTrees 3 tree
+    let features = nub $ concat $ map extractFeatures trees
     putStrLn $ show features
     readLibrary xs
 
@@ -54,20 +56,32 @@ buildTree _ = FNode (Id "unknown" 0 (Just (0,0))) []
 
 -- Extract subtrees with certain depth. Returns all possible trees as a list
 extractSubTrees :: Int -> FNode Id -> [FNode Id]
-extractSubTrees 1 (FNode id fs) = [(FNode id [])]
-extractSubTrees depth (FNode id fs) = [(FNode id (concat $ map (extractSubTrees (depth-1)) fs))] ++ (concat $ map (extractSubTrees depth) fs)
+extractSubTrees depth f@(FNode id fs) = [thisSubTree] ++ otherSubTrees
+    where
+        thisSubTree =  extractSubTree depth f --(FNode id (concat $ map (extractSubTrees (depth-1)) fs))
+        otherSubTrees = (concat $ map (extractSubTrees depth) fs)
+
+-- Extract one subtree
+extractSubTree :: Int -> FNode Id -> FNode Id
+extractSubTree 1 (FNode id _) = (FNode id [])
+extractSubTree depth (FNode id fs) = (FNode id (map (extractSubTree (depth-1)) fs))
 
 -- Extracts features from a tree
 extractFeatures :: FNode Id -> [String]
 extractFeatures (FNode id []) = [(idString id)]
 extractFeatures (FNode id f') = [(idString id)] ++ (map (\x -> (idString id) ++ "(" ++ x ++ ")") combos)
     where
-        arguments = concat $ map extractFeatures f'
+        arguments = map extractFeatures f'
         combos = combineArguments arguments
 
 -- Combines a list of arguments
-combineArguments :: [String] -> [String]
-combineArguments [] = []
-combineArguments (x:xs) = [x] ++ (map (\y -> x ++ " " ++ y) combinations) ++ combinations
-    where
-        combinations = combineArguments xs
+combineArguments :: [[String]] -> [String]
+combineArguments [xs] = xs
+combineArguments (xs:xss) = concat $ map (combine' xs) (combineArguments xss)
+
+-- Two helper functions for combineArguments
+combine' :: [String] -> String -> [String]
+combine' xs1 x = (map (mergeStrings x) xs1) ++ [x] ++ xs1
+
+mergeStrings :: String -> String -> String
+mergeStrings x1 x2 = x2 ++ ", " ++ x1
