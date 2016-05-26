e0db0a6024f60e006cff25a1daf65e15d93bfd80
Victor Lindhé <victorlindhe@Victors-MacBook-Air.local>
2016-05-26 10:24:56 +0200

    Lemma feature clustering tests

--- scripts/clusteringTests.py
+++ scripts/clusteringTests.py
@@ -4,22 +4,22 @@ def main():
   # Loop over all possible feature extraction schemes
   # TODO: any point with depth=0?
 
-  schemes = ["fs","fa"]
+  schemes = ["ls","la"]
   depths= [1,2,3]
   clusters = [2,8,20]
   n = len(depths)*len(clusters)*len(schemes)
 
   print "Processing %i combinations for clustering" % n
   
-  os.system("rm ./clustering/*")
+  os.system("rm ./clustering_lemmas/*")
 
   for s in schemes:
     for d in depths:
       for c in clusters:
         for n in range(0,10):
-          os.system("python ./scripts/unsupervised.py %i ./data/library.tiplib %i %s >> ./clustering/%s_d%i_c%i.txt" % (c,d,s,s,d,c))
+          os.system("python ./scripts/unsupervised.py %i ./data/library.tiplib %i %s >> ./clustering_lemmas/%s_d%i_c%i.txt" % (c,d,s,s,d,c))
           if n < 9:
-            os.system("echo \"####################################################################\n\" >> ./clustering/%s_d%i_c%i.txt " % (s,d,c))
+            os.system("echo \"####################################################################\n\" >> ./clustering_lemmas/%s_d%i_c%i.txt " % (s,d,c))
 
 if __name__ == '__main__':
   main()
